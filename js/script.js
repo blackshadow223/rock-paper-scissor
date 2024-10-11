24275de899137f0545a8511ebee83a0667b4f7d3
Black Shadow <mithabozdar2@gmail.com>
2024-10-11 17:12:31 +0500

    Fix a small bad code decision

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ function getComputerChoice() {
     // Create variable randomNumber to hold a random number from 0 to 5
     let randomNumber = Math.floor(Math.random() * 5);
     // Check and return Rock Paper or Scissor if the number happens to be 1, 2 or 3. If not, run the function again
-    if (randomNumber === 0 || randomNumber === 4 || randomNumber === 5) {
-        return getComputerChoice();
-    } else if (randomNumber === 1) {
+    if (randomNumber === 1) {
         return "rock";
     } else if (randomNumber === 2) {
         return "scissor";
-    } else {
+    } else if (randomNumber === 3) {
         return "paper";
+    } else {
+        return getComputerChoice();
     }
 }
 
